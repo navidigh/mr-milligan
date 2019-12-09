f12fe40462d3eceb903f6783c694962f5adb37bd
Ighani <319028@mcpsmd.org>
2019-12-09 09:28:50 -0500

    This series of code generates a number between 1 and 100, it gives you 10 chances to guess the number by making a while statement with a limit of 10 for the guess variable. it then uses the guesskyume variable to take your guess and compare it to the right number. and if it is higher or lower, you will get a message and get another chance to guess the number until you get it right or fail

--- game.py
+++ game.py
@@ -1,2 +1,21 @@
 import random
-guess = 0
+guesses = 0
+number = random.randint(1, 101)
+
+while guesses < 10:
+    guesskyume = int(input("I am well, thinking of a number. a number that happens to be in between the numerical values of 1 and 100" ))
+    guesses = guesses + 1
+
+    if guesskyume > number:
+        print("your guess is too high for my liking")
+
+    elif guesskyume < number:
+        print("your guess is too LOW for my liking")
+
+    else:
+        print("Good job! you guessed the number in " + str(guesses) + " tries!")
+        break
+
+
+    if guesses == 10:
+        print("Sorry, the number was actually" + str(number))
